73ea87a8dfb4a5e33c6d740bc0c5995ee024e6b0
blacksky3 <blacksky3@tuta.io>
2022-06-02 19:29:40 -0400

    fix

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@
 
 # gcc
 
-cd cacule && makepkg -s && rm -rf pkg/ src/ && cd ..
+cd 5.18/cacule && makepkg -s && rm -rf pkg/ src/ && cd ..
 
-cd cacule-rdb && makepkg -s && rm -rf pkg/ src/ && cd ..
+cd 5.18/cacule-rdb && makepkg -s && rm -rf pkg/ src/ && cd ..
 
 # clang
 
-#cd cacule && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ..
+#cd 5.18/cacule && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ..
 
-#cd cacule-rdb && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ..
+#cd 5.18/cacule-rdb && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ..
